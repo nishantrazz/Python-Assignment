4eb81110e7fdd0dbcdbab9119caa90468742ec44
nishantrazz <144988224+nishantrazz@users.noreply.github.com>
2023-10-16 13:47:46 +0530

    Add files via upload

---
+++ 11 prime number or not.py
@@ -0,0 +1,10 @@
+num = int(input("Enter an integer greater than 1: "))
+isprime = 1 #assuming that num is prime
+for i in range(2,num//2):
+    if (num%i==0):
+        isprime = 0
+        break
+if(isprime==1):
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
